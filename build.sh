e3152973936e5ed8eb7ba89bc9a713b2537d1a43
nillerusr <nillerusr@gmail.com>
2018-09-14 19:40:46 +0000

    update build.sh, fix make clean

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+<<<<<<< HEAD
 export KBUILD_BUILD_USER= #要设置的用户名
 export KBUILD_BUILD_HOST= #要设置的主机名
 export CROSS_COMPILE=~/gcc/bin/aarch64-linux-android- #指定你的gcc目录
 export ARCH=arm64 ARCH_MTK_PLATFORM=mt6735 #指定CPU和架构
 export TARGET=out #输出目录
-make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE tit_l01_defconfig #制作defconfig
-make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j64 #编译 -j后面是线程
+if ! [ -d $TARGET ];then mkdir $TARGET;fi
+if ! [ -f $TARGET/.config ];then make O=$TARGET ARCH=$ARCH titan_defconfig;fi
+make O=$TARGET ARCH=$ARCH $1 $2
